abd25343bf2c5402c05f3f4b2942550da4c9a4f0
shawn <49686577+xuyan0213@users.noreply.github.com>
2024-07-15 14:54:58 +0800

    Update pttime.js

--- pttime.js
+++ pttime.js
@@ -117,14 +117,14 @@ function doSign(timeout = 3 * 1000) {
                 msg += '\n⭐今日签到完成'
                 msg += `\n⭐最后签到时间：${time}\n⭐签到魔力值：${todayMoli}\n⭐连续签到天数：${day}天\n⭐总魔力值：${moli}`;
             }
-        } else if(data.indexOf('拒绝访问：高频刷新签到') > 0) {
+        } else if(data.indexOf('高频刷新签到') > 0) {
          $.log('高频刷新签到');
             console.log('高频刷新签到！！\n');
             msg += '\n 高频刷新签到，小心封号!'
         }else{
             
             $.log('签到失败');
-            console.log('签到失败失败！！\n');
+            console.log('签到失败！！\n');
             msg += '\n 签到失败!'
         }
     })
